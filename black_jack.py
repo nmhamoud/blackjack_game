d5d03217c0e3467ed2d92bcfa2a060db098d7b75
nmhamoud <nmhamoud@outlook.com>
2019-08-02 01:40:50 -0400

    initial commit

---
+++ black_jack.py
@@ -0,0 +1,21 @@
+'''
+This script simulates a simple version of the Black Jack game
+'''
+# a player:
+#     - take action: stand or hit
+#     - pick betting amount
+#     - track total money
+#     - alert of wins, losses or busts
+# cards:
+#     - values
+#     - suits
+# winnings:
+#     - player gets 21 (Black Jack)
+#     - player gets higher than dealer
+#     - dealer exceeds 21 (busted)
+# procedure:
+#     - player receive 2 cards
+#     - dealer shows 1 card only
+#     - player hits or stands
+#     - after standing the dealer play until his card values exceed 16
+ 
